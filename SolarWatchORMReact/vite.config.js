477118ce2fc36d80a497e3be738c56a41308d612
Tommer-04 <tothtom1205@gmail.com>
2024-09-02 15:55:50 +0200

    Backend placeholder for react proxy

--- SolarWatchORMReact/vite.config.js
+++ SolarWatchORMReact/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5035/',
+        target: '[BACKEND_URL]',
         changeOrigin: true,
         secure: false
       }
